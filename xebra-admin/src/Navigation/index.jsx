819b3b0713c427012dc23bae45975df3d63e1857
SyedKamranGH <syed.kam.ahmed@gmail.com>
2022-09-30 15:36:37 +0500

    Confirm rest password and change password

--- xebra-admin/src/Navigation/index.jsx
+++ xebra-admin/src/Navigation/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import UserManagement from "../Components/layouts/UserManagement";
+import ChangePassword from "../Screens/UserManagement/ChangePassword";
 import ForgotPassword from "../Screens/UserManagement/ForgotPassword";
 import Home from "../Screens/UserManagement/Home";
 import Login from "../Screens/UserManagement/Login";
+import RestPassword from "../Screens/UserManagement/ResetPassword";
 
 const Navigation = () => {
 	return (
@@ -12,6 +14,8 @@ const Navigation = () => {
 				<Route index element={<Home />} />
 				<Route path="login" element={<Login />} />
 				<Route path="forgotPassword" element={<ForgotPassword />} />
+				<Route path="restPassword" element={<RestPassword />} />
+				<Route path="changePassword" element={<ChangePassword />} />
 			</Route>
 		</Routes>
 	);
